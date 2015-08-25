1e1999ea97e0996259495e116a6460165666f960
Rory Hunter <rory.hunter@blackpepper.co.uk>
2015-08-25 15:37:12 +0100

    sshtun tweaks, and tag everything with the registry

--- build.sh
+++ build.sh
@@ -3,15 +3,17 @@
 set -e
 
 build_container() {
-	NAME="blackpepper/$1"
-	DIR="$2"
-	VERSION="$3"
+	local NAME="blackpepper/$1"
+	local DIR="$2"
+	local VERSION="$3"
 
 	echo "Building $NAME..."
 	docker build -t $NAME $DIR
+	docker tag $NAME:latest registry.blackpepper.co.uk/$NAME:latest
 
 	if [ -n "$VERSION" ]; then
 		docker tag -f $NAME:latest $NAME:$VERSION
+		docker tag -f $NAME:latest registry.blackpepper.co.uk/$NAME:$VERSION
 	fi
 }
 
--- sshtun/run.sh
+++ sshtun/run.sh
@@ -8,6 +8,5 @@ docker run \
 	--detach=true \
 	--publish=2222:22 \
 	--expose=6636 \
-	--volume=/etc/ssh/sshd_config:/etc/ssh/sshd_config \
 	--volume=/root/ambassador/blackpepper.pub:/etc/ssh/authorized_keys/blackpepper \
-	blackpepper/sshtun
+	registry.blackpepper.co.uk/blackpepper/sshtun
